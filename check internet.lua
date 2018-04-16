fbaf47ea3d89e279437cff5685afc7c0442cebae
Qugurun <38307979+Qugurun@users.noreply.github.com>
2018-04-16 20:10:24 +0500

    Update check internet.lua

--- check internet.lua
+++ check internet.lua
@@ -1,22 +1,11 @@
-local socket = require("socket")    
-local internet = socket.tcp()
-internet:settimeout(5000)                 
-          
-local function testNetworkConnection()
-	local internetResult = internet:connect("www.google.com", 80)       
-	
-	if not(internetResult == nil) then
+local socket = require "socket"
+
+function NetworkConnection()
+   local internet = socket.tcp()
+   internet:settimeout( 2 )
+    if not(internet:connect("www.google.com", 80) == nil) then
 		return true
 	else
 		return false
-	end   
-	
-	internet:close()
-	internet = nil
-end
-
-if testNetworkConnection() == false then
-	print("Internet false")
-else
-	print("Internet true")	
+	end  
 end
